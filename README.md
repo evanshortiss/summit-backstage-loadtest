f29ed14f54dd75201b33a2eca5213fbe2fa52562
Evan Shortiss <evanshortiss@gmail.com>
2023-05-19 09:32:01 -0700

    let's goooo 🚀

---
+++ README.md
@@ -0,0 +1,11 @@
+# Usage
+
+Tested with Node.js v18.
+
+```bash
+# Install deps
+npm ci
+
+# Simulate a single user setting up the map application
+APP_TYPE=map-app USER_COUNT=1 npx ts-node index.ts
+```
---
+++ index.ts
@@ -0,0 +1,82 @@
+import { execa } from "execa"
+import { existsSync, mkdirSync, readdirSync, readFileSync, writeFileSync } from "fs"
+import { join } from "path"
+import * as url from 'url';
+import env from 'env-var'
+import log from 'barelog'
+
+export type ApplicationName = 'map-app'|'gateway'|'backend'
+export type UsernameString = `testuser-${number}`
+
+const USER_COUNT = env.get('USER_COUNT').default(1).asIntPositive()
+const APP_TYPE = env.get('APP_TYPE').required().asEnum(['map-app','gateway','backend'])
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+async function main () {
+  for (let i = 0; i < USER_COUNT; i++) {
+    const user: UsernameString = `testuser-${i}`
+    const delayMs = Math.round(Math.random() * 60000)
+
+    log(`${user} will create the ${APP_TYPE} in ${delayMs / 1000} seconds...`)
+
+    ;(function (user: UsernameString) {
+      delay(delayMs)
+        .then(() => {
+          log(`${user} applying GitOps resources for ${APP_TYPE}`)
+          applyApplicationTypeForUser(APP_TYPE, user)
+        })
+        .catch((e) => {
+          log('error deploying application', e)
+        })
+    })(user)
+  }
+}
+
+function delay (millis: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), millis)
+  })
+}
+
+async function applyApplicationTypeForUser (app: ApplicationName, user: UsernameString) {
+  const userPath = join('/tmp', user)
+
+  if (!existsSync(userPath)) {
+    mkdirSync(userPath)
+  }
+
+  const files = readdirSync(join(__dirname, 'applications'))
+  
+  const applications = files
+    .filter(f => f.startsWith(APP_TYPE))
+    .map((f) => {
+      const yaml = readFileSync(join(__dirname, 'applications', f), 'utf-8').replace(/\{\{username\}\}/ig, user)
+      const yamlPath = join('/tmp', user, f)
+
+      writeFileSync(join('/tmp', user, f), yaml)
+
+      return yamlPath
+    })
+
+  // Create args for oc, i.e "apply -f $A -f $B"
+  const args = applications.reduce<string[]>((prev, cur) => {
+    prev.push('-f', cur)
+    return prev
+  }, ['apply'])
+
+  // Appened the GitOps namespace to args
+  args.push('-n', 'janus-argocd')
+
+  log(`${user} applying the following resources via oc:`, args)
+
+  try {
+    // Execute the oc apply command 
+    const { stdout } = await execa('oc', args)
+    log(`${user} apply success`, stdout)
+  } catch (e) {
+    log(`${user} apply error`, e)
+  }
+}
+
+main()
